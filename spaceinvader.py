5db6a40c0a0d62370e0bc5e526592d95c2ecf5d8
Christian Saunders <noname45688@gmail.com>
2020-04-25 22:24:22 -0400

    added movement

--- spaceinvader.py
+++ spaceinvader.py
@@ -1,5 +1,10 @@
 import pygame
 
+
+def player(img,x,y):
+    window.blit(img,(x,y))
+
+
 pygame.init()
 
 # Creates game window
@@ -16,18 +21,29 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load('space-invaders.png')
 playerX = 370
 playerY = 480
-
-def player():
-    window.blit(playerImg,(playerX,playerY))
-
+playerX_change = 0
 
 # Game loop
 running = True
 while running:
+    # Background: (RGB)
+    window.fill((0, 255, 255))
+
+    # Event check and execute
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-    # Background: (RGB)
-    window.fill((0, 255, 255))
-    player()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerX_change = 0.3
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerX_change = 0
+    
+    # Player:
+    playerX += playerX_change
+    player(playerImg,playerX,playerY)
+    # Update original background
     pygame.display.update()
